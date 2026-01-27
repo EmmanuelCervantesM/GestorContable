f1bfdc5feeb60c81e63f493c8836a8a64377d9d3
joaquin-candia <joaquin.candia@ebiw.mx>
2026-01-26 21:59:28 -0600

    añade un filtro en la busqueda de la entidad

--- RN_GC/src/java/mx/com/rocketnegocios/entities/RnGcNomPeriodonominaTbl.java
+++ RN_GC/src/java/mx/com/rocketnegocios/entities/RnGcNomPeriodonominaTbl.java
@@ -45,7 +45,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     , @NamedQuery(name = "RnGcNomPeriodonominaTbl.findByMesPeriodo", query = "SELECT r FROM RnGcNomPeriodonominaTbl r WHERE r.mesPeriodo = :mesPeriodo")
     , @NamedQuery(name = "RnGcNomPeriodonominaTbl.findByNumMesPeriodo", query = "SELECT r FROM RnGcNomPeriodonominaTbl r WHERE r.numMesPeriodo = :numMesPeriodo")
     , @NamedQuery(name = "RnGcNomPeriodonominaTbl.findByEstatus", query = "SELECT r FROM RnGcNomPeriodonominaTbl r WHERE r.estatus = :estatus")
-    , @NamedQuery(name = "RnGcNomPeriodonominaTbl.findByNomina", query = "SELECT r FROM RnGcNomPeriodonominaTbl r WHERE r.nominaId = :nominaId")})
+    , @NamedQuery(name = "RnGcNomPeriodonominaTbl.findByNomina", query = "SELECT r FROM RnGcNomPeriodonominaTbl r WHERE r.nominaId.id = :nominaId")})
 public class RnGcNomPeriodonominaTbl implements Serializable {
 
     private static final long serialVersionUID = 1L;
